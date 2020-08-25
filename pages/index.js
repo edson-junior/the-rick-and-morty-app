0cb1386244677f3ba5ff734290fb08828ea87879
Edson Junior <soneddjr@gmail.com>
2020-08-26 00:10:16 +0200

    first basic setup

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+
+const Homepage = () => (
+  <div>
+    <Header />
+    <h1>First Page!</h1>
+  </div>
+)
+
+export default Homepage
